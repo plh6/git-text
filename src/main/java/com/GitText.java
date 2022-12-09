787bbe12b09da8b4c4beb487328aa33891da69e3
plh <plh@123.com>
2022-12-09 20:20:50 +0800

    third commit

--- src/main/java/com/GitText.java
+++ src/main/java/com/GitText.java
@@ -4,5 +4,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git........");
+        System.out.println("hello Git........3");
     }
 }
