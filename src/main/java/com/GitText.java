61f1efb8dad383cc8dbd692beb83f6c886c468a8
plh <plh@123.com>
2022-12-09 20:04:32 +0800

    second commit

--- src/main/java/com/GitText.java
+++ src/main/java/com/GitText.java
@@ -3,5 +3,6 @@ package com;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git........");
     }
 }
